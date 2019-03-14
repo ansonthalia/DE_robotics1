e4e497d82ab3a1dde783357108e9de451799fc53
omjh <48054839+omjh@users.noreply.github.com>
2019-03-14 15:45:31 +0000

    Update Model_Spawn.py

--- Final_Submission/Model_Spawn.py
+++ Final_Submission/Model_Spawn.py
@@ -22,6 +22,10 @@ def load_tables(table_pose1=Pose(position=Point(x=0.9, y=0, z=0.73)),
                        table_reference_frame2="world",
                        table_pose3=Pose(position=Point(x=0.6, y=-1.1, z=0.73)),
                        table_reference_frame3="world"):
+    
+   '''This function loads the tables in specific locations. Import to note the 
+   directories called for the model URDF files'''
+
     #Get models from directory
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
     #Load 1
@@ -61,6 +65,11 @@ def load_tables(table_pose1=Pose(position=Point(x=0.9, y=0, z=0.73)),
 def load_UP(block_no, block_side, block_pose_left=Pose(position=Point(x=0.1, y= 0.7, z=0.74)),
                        block_reference_frame="world",
                        block_pose_right=Pose(position=Point(x=0.1, y= -0.7, z=0.74))):
+    
+    
+   '''This function loads the Upright bricks in specific locations (left and right). Import to note the 
+   directories called for the model URDF files'''
+    
     # Get Models' Path
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
     block_xml = ''
@@ -91,6 +100,10 @@ def load_UP(block_no, block_side, block_pose_left=Pose(position=Point(x=0.1, y= 
 def load_Flat(block_no, block_side, block_pose_left=Pose(position=Point(x=0.3, y= 0.7, z=0.73)),
                        block_reference_frame="world",
                        block_pose_right=Pose(position=Point(x=0.3, y=-0.7, z=0.73))):
+    
+   '''This function loads the Flat bricks in specific locations. Import to note the 
+   directories called for the model URDF files'''
+
     # Get Models' Path
     model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
     block_xml = ''
